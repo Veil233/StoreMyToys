16431c84c385549e8d3239d757f814b5aa4899d9
taozhuowei <876593497@qq.com>
2020-04-18 17:31:15 +0800

    commit LoginForm 2

--- LoginForm/js/ajax.js
+++ LoginForm/js/ajax.js
@@ -1,9 +1,12 @@
+/**
+ * 封装ajax
+ * @param option{ url , data , type , success() }
+ * data=param1&param2&...
+ */
 function ajax(option) {
     option=option || {}//如果没有设置默认为空对象
     option.type=option.type || "get"//默认请求格式为get
-    option.dataType=option.dataType || "json"//默认响应数据格式为json
 
-    var params=formatParams(option.data)//处理数据
     var xhr
 
     //创建
@@ -16,31 +19,23 @@ function ajax(option) {
 
     //连接&发送
     if ( option.type == 'get' ){
-        xhr,open("get" , `${option.url}?${params}` , true )
+        xhr.open("get", `${option.url}?{option.data}` , true )
         xhr.send()
     }
     else if ( option.type == 'post' ){
         xhr.open("post" , option.url , true)
         xhr.setRequestHeader("Content-Type" , "application/x-www-form-urlencoded")
-        xhr.send(params)
+        xhr.send(option.data)
     }
 
     //接收
     xhr.onreadystatechange=function () {
         if ( xhr.readyState == 4 && xhr.status == 200 ){
-            option.success( xhr.responseText)
+            console.log("response from server : "+xhr.responseText)
+            option.success( xhr.responseText )
         }
         else {
             console.log(xhr.error)
         }
     }
-}
-
-function formatParams(data) {
-    var dataArr=[]
-    for (let key in data){
-        dataArr.push( `${ encodeURIComponent(key) }=${ encodeURIComponent( dataArr[name] ) }` )
-    }
-    dataArr.push(`id=${Math.random()}`)
-    return dataArr.join("&")
 }
